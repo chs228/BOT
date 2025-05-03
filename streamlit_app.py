54d3c9142daf7340cc33cd8d341be7b5c6454e9b
chs228 <chsubhash22@gmail.com>
2025-05-03 18:54:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -319,7 +319,6 @@ def evaluate_answer(question, answer, expected_keywords):
     # if not genai:
     #     return {"score": 0, "feedback": "Gemini API not available.", "missing_concepts": expected_keywords}
     # GEMINI_API_KE
-    # Y = "AIzaSyDlb0thGyUHOBuT5bmv9a8QCkg-UX5iMgY"
     # if not GEMINI_API_KEY:
     #     return {"score": 0, "feedback": "No Gemini API key.", "missing_concepts": expected_keywords}
     try:
