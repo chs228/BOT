022c0763b294022070d148064d8467afb792d2b7
chs228 <chsubhash22@gmail.com>
2025-05-03 19:02:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ from fpdf import FPDF
 
 # Firebase configuration
 firebase_config = {
-    "apiKey": st.secerts["FIREBASE_API"],
+    "apiKey": api=st.secrets["FIREBASE_API"],
     "authDomain": "client-2bbfc.firebaseapp.com",
     "databaseURL": "https://client-2bbfc-default-rtdb.firebaseio.com",
     "projectId": "client-2bbfc",
